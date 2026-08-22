6c617b398449b264b0a547ddea986a1e0d80b80c
Mewen Batty <scorpionem86@gmail.com>
2026-08-22 17:00:13 +0200

    feat: input text, slider, toggle

--- inc/ui/UI.hpp
+++ inc/ui/UI.hpp
@@ -24,7 +24,8 @@ namespace UI
 
     bool    button(const std::string& label, vec2i pos, vec2i size, Anchor anchor = Anchor::CENTER);
     void	text(const std::string& label, vec2i pos, Anchor anchor = Anchor::CENTER);
-    // bool	toggle(const std::string& label, vec2i pos, vec2i size, bool default_state = false, Anchor anchor = Anchor::CENTER);
-    // bool    text(const std::string& label, vec2i pos, vec2i size, ...); // va args
-    // bool    text_field(const std::string& label, std::string& input, vec2i pos, vec2i size); // va args
-};
+    // takes state of the toggle as input, returns true when toggle is clicked
+    bool	toggle(const std::string& label, bool& state, vec2i pos, vec2i size, Anchor anchor = Anchor::CENTER);
+    bool    input(const std::string& label, std::string& input, vec2i pos, vec2i size, Anchor anchor = Anchor::CENTER);
+    bool	slider(const std::string& label, int& input, int min, int max, vec2i pos, vec2i size, Anchor anchor = Anchor::CENTER);
+}
