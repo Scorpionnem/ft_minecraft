c7c6a008c4b1fec5b7431bef44a084ee155b712e
Mewen Batty <scorpionem86@gmail.com>
2026-09-08 19:13:09 +0200

    feat: aabb mesh and rendering, temporary in the class, need to figure out better architecture

--- inc/utils/AABB.hpp
+++ inc/utils/AABB.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "math.hpp"
+#include "render/Mesh.hpp"
+#include "render/Camera.hpp"
 
 template <std::size_t N, typename T>
 struct  AABB
@@ -23,6 +25,40 @@ struct  AABB
                 return (false);
         return (true);
     }
+
+    static void	gen_aabb3_mesh(Mesh& mesh) requires (N == 3)
+	{
+		mesh.set_sizeof_layout(2 * sizeof(vec3f));
+		mesh.add_vertex_layout(0, 3, GL_FLOAT, 0);
+		mesh.add_vertex_layout(1, 3, GL_FLOAT, sizeof(vec3f));
+
+		vec3f vertices[] = {
+			{0,0,0}, {0,0,0}, {1,0,0}, {0,0,0},
+			{0,0,0}, {0,0,0}, {0,1,0}, {0,0,0},
+			{0,0,0}, {0,0,0}, {0,0,1}, {0,0,0},
+			{0,1,0}, {0,0,0}, {1,1,0}, {0,0,0},
+			{0,1,0}, {0,0,0}, {0,1,1}, {0,0,0},
+			{1,1,1}, {0,0,0}, {0,1,1}, {0,0,0},
+			{1,1,1}, {0,0,0}, {1,0,1}, {0,0,0},
+			{1,1,1}, {0,0,0}, {1,1,0}, {0,0,0},
+			{0,1,1}, {0,0,0}, {0,0,1}, {0,0,0},
+			{0,0,1}, {0,0,0}, {1,0,1}, {0,0,0},
+			{1,0,1}, {0,0,0}, {1,0,0}, {0,0,0},
+			{1,1,0}, {0,0,0}, {1,0,0}, {0,0,0},
+		};
+
+		mesh.add_vertex_data(reinterpret_cast<u8*>(vertices), sizeof(vertices));
+	}
+
+	template <typename TT>
+	static void draw_aabb3(AABB<3, TT> aabb, Camera& cam, Shader& shader, Mesh& mesh)
+	{
+		shader.bind();
+		shader.setMat4("uProj", cam.getProjectionMatrix());
+		shader.setMat4("uView", cam.getViewMatrix());
+		shader.setMat4("uModel", mat4f::translate(aabb.min) * mat4f::scale(aabb.max));
+		mesh.draw(GL_LINES);
+	}
 };
 
 using aabb2i = AABB<2, int>;
